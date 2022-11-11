5dbba223bcc089a8c97e70cfa623b88668f9d944
windybird <windybird@qq.com>
2022-11-11 13:08:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,24 +10,29 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-#1. Modify default IP
+# 1. Modify default IP
 sed -i 's/192.168.1.1/10.10.10.13/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.$((addr_offset++)).1/10.10.$((addr_offset++)).13/g' package/base-files/files/bin/config_generate
 
-#2. change the login password
+# 2. change the login password
 sed -i '/root::0:0:99999:7:::/s/^/#/' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/root::0:0:99999:7:::/root:$1$iZM.01X5$xfeRwcqbhN\/60\/2SUPwDc\/:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-#3. Import external feeds - JerryKuKu Argon
+# 3. Import external feeds - JerryKuKu Argon
 rm -rf feeds/luci/themes/luci-theme-bootstrap-mod
 rm -rf feeds/luci/themes/luci-theme-material
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon-mod
 rm -rf feeds/luci/themes/luci-theme-netgear
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+# 4. add luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
 
-#4. defaut themes
+# 5. defaut themes
 sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+#. add luci-app-oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
